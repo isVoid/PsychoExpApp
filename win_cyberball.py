3e7234570ce884d68a1641fc07786600aed75670
michael wang <michaelwang0905@gmail.com>
2021-05-08 17:12:42 -0700

    Clean up file loads

--- win_cyberball.py
+++ win_cyberball.py
@@ -61,61 +61,12 @@ def set_face_pair(imstim1, imstim2):
 
 #提示刺激を準備
 wait = make_actimagestim('./stim/start.bmp')
-
-LS1 = make_actimagestim('./stim/1toY(1).bmp')
-LS2 =  make_actimagestim('./stim/1toY(2).bmp')
-LS3 =  make_actimagestim('./stim/1toY(3).bmp')
-LS4 =  make_actimagestim('./stim/1toY(4).bmp')
-LS5 =  make_actimagestim('./stim/1toY(5).bmp')
-LS6 =  make_actimagestim('./stim/1toY(6).bmp')
-LS7 =  make_actimagestim('./stim/1toY(7).bmp')
-LS8 =  make_actimagestim('./stim/1toY(8).bmp')
-
-LR1 =  make_actimagestim('./stim/1to2(1).bmp')
-LR2 =  make_actimagestim('./stim/1to2(2).bmp')
-LR3 =  make_actimagestim('./stim/1to2(3).bmp')
-LR4 =  make_actimagestim('./stim/1to2(4).bmp')
-LR5 =  make_actimagestim('./stim/1to2(5).bmp')
-LR6 =  make_actimagestim('./stim/1to2(6).bmp')
-LR7 =  make_actimagestim('./stim/1to2(7).bmp')
-LR8 =  make_actimagestim('./stim/1to2(8).bmp')
-LR9 =  make_actimagestim('./stim/1to2(9).bmp')
-
-RS1 =  make_actimagestim('./stim/2toY(1).bmp')
-RS2 =  make_actimagestim('./stim/2toY(2).bmp')
-RS3 =  make_actimagestim('./stim/2toY(3).bmp')
-RS4 =  make_actimagestim('./stim/2toY(4).bmp')
-RS5 =  make_actimagestim('./stim/2toY(5).bmp')
-RS6 =  make_actimagestim('./stim/2toY(6).bmp')
-RS7 =  make_actimagestim('./stim/2toY(7).bmp')
-RS8 =  make_actimagestim('./stim/2toY(8).bmp')
-
-RL1 =  make_actimagestim('./stim/2to1(1).bmp')
-RL2 =  make_actimagestim('./stim/2to1(2).bmp')
-RL3 =  make_actimagestim('./stim/2to1(3).bmp')
-RL4 =  make_actimagestim('./stim/2to1(4).bmp')
-RL5 =  make_actimagestim('./stim/2to1(5).bmp')
-RL6 =  make_actimagestim('./stim/2to1(6).bmp')
-RL7 =  make_actimagestim('./stim/2to1(7).bmp')
-RL8 =  make_actimagestim('./stim/2to1(8).bmp')
-RL9 =  make_actimagestim('./stim/2to1(9).bmp')
-
-SL1 =  make_actimagestim('./stim/Yto1(1).bmp')
-SL2 =  make_actimagestim('./stim/Yto1(2).bmp')
-SL3 =  make_actimagestim('./stim/Yto1(3).bmp')
-SL4 =  make_actimagestim('./stim/Yto1(4).bmp')
-SL5 =  make_actimagestim('./stim/Yto1(5).bmp')
-SL6 =  make_actimagestim('./stim/Yto1(6).bmp')
-SL7 =  make_actimagestim('./stim/Yto1(7).bmp')
-
-SR1 =  make_actimagestim('./stim/Yto2(1).bmp')
-SR2 =  make_actimagestim('./stim/Yto2(2).bmp')
-SR3 =  make_actimagestim('./stim/Yto2(3).bmp')
-SR4 =  make_actimagestim('./stim/Yto2(4).bmp')
-SR5 =  make_actimagestim('./stim/Yto2(5).bmp')
-SR6 =  make_actimagestim('./stim/Yto2(6).bmp')
-SR7 =  make_actimagestim('./stim/Yto2(7).bmp')
-
+LS_image_stim = [make_actimagestim(x) for x in sorted(glob.glob('./stim/1toY*'))]
+LR_image_stim = [make_actimagestim(x) for x in sorted(glob.glob('./stim/1to2*'))]
+RS_image_stim = [make_actimagestim(x) for x in sorted(glob.glob('./stim/2toY*'))]
+RL_image_stim = [make_actimagestim(x) for x in sorted(glob.glob('./stim/2to1*'))]
+SL_image_stim = [make_actimagestim(x) for x in sorted(glob.glob('./stim/Yto1*'))]
+SR_image_stim = [make_actimagestim(x) for x in sorted(glob.glob('./stim/Yto2*'))]
 conw = visual.ImageStim(myWin, image=u'./stim/connectingwindow.bmp', mask = None, pos = (0,0), size = [0.4, 0.25])
 
 # Face images paths
@@ -138,70 +89,32 @@ def frame(imgstims, duration=0.2):
     myWin.flip()
     core.wait(duration)
 
+def animation(img_stims, l_face, r_face, frame_duration, randomize_first_frame=True):
+    start = 0
+    if randomize_first_frame:
+        frame([l_face, r_face, img_stims[0]], duration=random.uniform(0.5, 1.5))
+        start = 1
+    for img in img_stims[start:]:
+        frame([l_face, r_face, img], duration=frame_duration)
+
 #Lは参加者から見て左、Sは参加者、Rは参加者から見て右。LSは、左の人から参加者にボールが投げられるアクションを指す
 def LS(l_face, r_face):
-    r = random.uniform(0.5, 1.5) 
-    frame([l_face, r_face, LS1], duration=r)
-    frame([l_face, r_face, LS2], duration=anim_frame_duration)
-    frame([l_face, r_face, LS3], duration=anim_frame_duration)
-    frame([l_face, r_face, LS4], duration=anim_frame_duration)
-    frame([l_face, r_face, LS5], duration=anim_frame_duration)
-    frame([l_face, r_face, LS6], duration=anim_frame_duration)
-    frame([l_face, r_face, LS7], duration=anim_frame_duration)
-    frame([l_face, r_face, LS8], duration=anim_frame_duration)
+    animation(LS_image_stim, l_face, r_face, anim_frame_duration, randomize_first_frame=True)
 
 def LR(l_face, r_face):
-    r = random.uniform(0.5, 1.5)
-    frame([l_face, r_face, LR1], duration=r)
-    frame([l_face, r_face, LR2], duration=anim_frame_duration)
-    frame([l_face, r_face, LR3], duration=anim_frame_duration)
-    frame([l_face, r_face, LR4], duration=anim_frame_duration)
-    frame([l_face, r_face, LR5], duration=anim_frame_duration)
-    frame([l_face, r_face, LR6], duration=anim_frame_duration)
-    frame([l_face, r_face, LR7], duration=anim_frame_duration)
-    frame([l_face, r_face, LR8], duration=anim_frame_duration)
-    frame([l_face, r_face, LR9], duration=anim_frame_duration)
+    animation(LR_image_stim, l_face, r_face, anim_frame_duration, randomize_first_frame=True)
 
 def RS(l_face, r_face):
-    r = random.uniform(0.5, 1.5)
-    frame([l_face, r_face, RS1], duration=r)
-    frame([l_face, r_face, RS2], duration=anim_frame_duration)
-    frame([l_face, r_face, RS3], duration=anim_frame_duration)
-    frame([l_face, r_face, RS4], duration=anim_frame_duration)
-    frame([l_face, r_face, RS5], duration=anim_frame_duration)
-    frame([l_face, r_face, RS6], duration=anim_frame_duration)
-    frame([l_face, r_face, RS7], duration=anim_frame_duration)
-    frame([l_face, r_face, RS8], duration=anim_frame_duration)
+    animation(RS_image_stim, l_face, r_face, anim_frame_duration, randomize_first_frame=True)
 
 def RL(l_face, r_face):
-    r = random.uniform(0.5, 1.5)
-    frame([l_face, r_face, RL1], duration=r)
-    frame([l_face, r_face, RL2], duration=anim_frame_duration)
-    frame([l_face, r_face, RL3], duration=anim_frame_duration)
-    frame([l_face, r_face, RL4], duration=anim_frame_duration)
-    frame([l_face, r_face, RL5], duration=anim_frame_duration)
-    frame([l_face, r_face, RL6], duration=anim_frame_duration)
-    frame([l_face, r_face, RL7], duration=anim_frame_duration)
-    frame([l_face, r_face, RL8], duration=anim_frame_duration)
-    frame([l_face, r_face, RL9], duration=anim_frame_duration)
+    animation(RL_image_stim, l_face, r_face, anim_frame_duration, randomize_first_frame=True)
 
 def SL(l_face, r_face):
-    frame([l_face, r_face, SL1], duration=anim_frame_duration)
-    frame([l_face, r_face, SL2], duration=anim_frame_duration)
-    frame([l_face, r_face, SL3], duration=anim_frame_duration)
-    frame([l_face, r_face, SL4], duration=anim_frame_duration)
-    frame([l_face, r_face, SL5], duration=anim_frame_duration)
-    frame([l_face, r_face, SL6], duration=anim_frame_duration)
-    frame([l_face, r_face, SL7], duration=anim_frame_duration)
+    animation(SL_image_stim, l_face, r_face, anim_frame_duration, randomize_first_frame=False)
 
-def SR(l_face, r_face):
-    frame([l_face, r_face, SR1], duration=anim_frame_duration)
-    frame([l_face, r_face, SR2], duration=anim_frame_duration)
-    frame([l_face, r_face, SR3], duration=anim_frame_duration)
-    frame([l_face, r_face, SR4], duration=anim_frame_duration)
-    frame([l_face, r_face, SR5], duration=anim_frame_duration)
-    frame([l_face, r_face, SR6], duration=anim_frame_duration)
-    frame([l_face, r_face, SR7], duration=anim_frame_duration)    
+def SR(l_face, r_face):  
+    animation(SR_image_stim, l_face, r_face, anim_frame_duration, randomize_first_frame=False)
 
 def instruction1():
     myWin.setMouseVisible(True)
@@ -344,10 +257,27 @@ def practice():
 #####################################################################################
 # Session template
 
-# position: 0 self, 1 left, 2 right
-
 def session(total_passes, a = 0.5):
-    # winsound.Beep(523, 5000)
+    """
+    Main loop for a session
+
+    Parameter
+    ---------
+    total_passes: int
+    Controls the total number of passes in this session
+
+    a: float [0, 1.0]
+    Controls the distribution of probability passing to user, see
+    `prob_model` for usage
+
+    Return
+    ------
+    None
+    """
+    if not 0 <= a and a <= 1.0:
+        raise ValueError("Invalid value a.")
+
+    winsound.Beep(523, 5000)
 
     emotions = list(face.keys())
     l_emo, r_emo = random.choice(emotions), random.choice(emotions)
@@ -358,7 +288,7 @@ def session(total_passes, a = 0.5):
     num_Spass = 0
     num_Spass_totl = round(total_passes / 3)
     if cur_pos == 0:
-        frame([l_face, r_face, LS1])
+        frame([l_face, r_face, LS_image_stim[0]])
     for rnd in range(total_passes):
         if cur_pos == 0:
             (selectKey, react_time) = getKeyboardResponse(['left', 'right'])
@@ -388,10 +318,10 @@ def session(total_passes, a = 0.5):
                     RL(l_face, r_face)
                     cur_pos = 1
     m = int(total_passes / 3)
-    # winsound.Beep(523, 500)
-    # between()
+    winsound.Beep(523, 500)
+    between()
 
 if __name__ == "__main__":
-    # user_id_input()
-    # practice()
+    user_id_input()
+    practice()
     session(60, 0.5)
